c3c8ba34344b66c0ffdb767ea2e5bc439771e24b
Tumidanski Justin D <Justin.Tumidanski@dematic.com>
2021-01-07 11:38:30 -0500

    Add constant for accessing rest service.

---
+++ tds-api/src/main/java/com/atlas/tds/constant/RestConstants.java
@@ -0,0 +1,8 @@
+package com.atlas.tds.constant;
+
+public final class RestConstants {
+   private RestConstants() {
+   }
+
+   public static final String SERVICE = "tds";
+}
--- tds-base/src/main/java/com/atlas/tds/Server.java
+++ tds-base/src/main/java/com/atlas/tds/Server.java
@@ -3,15 +3,12 @@ package com.atlas.tds;
 import java.net.URI;
 
 import com.atlas.shared.rest.RestServerFactory;
-import com.atlas.shared.rest.RestService;
 import com.atlas.shared.rest.UriBuilder;
-import org.glassfish.grizzly.http.server.HttpServer;
-
-import database.PersistenceManager;
+import com.atlas.tds.constant.RestConstants;
 
 public class Server {
    public static void main(String[] args) {
-      URI uri = UriBuilder.host(RestService.TOPIC_DISCOVERY).uri();
+      URI uri = UriBuilder.host(RestConstants.SERVICE).uri();
       RestServerFactory.create(uri, "com.atlas.tds.rest");
    }
 }
